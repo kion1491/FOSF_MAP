b2e73db4314e2b49a0069c425577faf972860b20
kion1491 <43998132+kion1491@users.noreply.github.com>
2019-07-19 23:31:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,46 +4,40 @@ navigator.geolocation.getCurrentPosition((position) => {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     var positionArray = [latitude, longitude]
+    var searchKeyword = "부천 고강동 동물병원";
+
+    // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 
-    var mapContainer = document.getElementById('map'); // 지도를 표시할 div 
-    var mapOption = {
-        center: new kakao.maps.LatLng(positionArray[0], positionArray[1]), // 지도의 중심좌표
-        level: 1 // 지도의 확대 레벨
-    };
-    var searchKeyword = "동물병원";
-
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+        mapOption = {
+            center: new kakao.maps.LatLng(positionArray[0], positionArray[1]), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
 
     // 지도를 생성합니다    
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
     // 장소 검색 객체를 생성합니다
-    var ps = new kakao.maps.services.Places();
+    var ps = new kakao.maps.services.Places(map);
 
     // 키워드로 장소를 검색합니다
-    ps.keywordSearch(searchKeyword, placesSearchCB);
+    //ps.keywordSearch(searchKeyword, placesSearchCB);
+
+    // 카테고리로 은행을 검색합니다
+    ps.categorySearch('BK9', placesSearchCB, { useMapBounds: true });
 
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB(data, status, pagination) {
         if (status === kakao.maps.services.Status.OK) {
-
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-            // LatLngBounds 객체에 좌표를 추가합니다
-            var bounds = new kakao.maps.LatLngBounds();
-
             for (var i = 0; i < data.length; i++) {
                 displayMarker(data[i]);
-                bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
             }
-
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-            map.setBounds(bounds);
         }
     }
 
     // 지도에 마커를 표시하는 함수입니다
     function displayMarker(place) {
-
         // 마커를 생성하고 지도에 표시합니다
         var marker = new kakao.maps.Marker({
             map: map,
